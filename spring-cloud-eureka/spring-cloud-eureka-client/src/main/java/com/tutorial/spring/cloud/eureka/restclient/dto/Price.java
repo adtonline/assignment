20a48bf830bde4c141eca2a4b829ff383445f4fb
javaee2015 <ocbu1989@yahoo.com>
2018-07-23 22:38:41 +0700

    BE Assignment: microservice with eureka

---
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/tutorial/spring/cloud/eureka/restclient/dto/Price.java
@@ -0,0 +1,6 @@
+package com.tutorial.spring.cloud.eureka.restclient.dto;
+
+public class Price {
+
+	private String dateClose;
+}
---
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/tutorial/spring/cloud/eureka/restclient/dto/Ticker.java
@@ -0,0 +1,7 @@
+package com.tutorial.spring.cloud.eureka.restclient.dto;
+
+public class Ticker {
+	
+	private String close;
+
+}
